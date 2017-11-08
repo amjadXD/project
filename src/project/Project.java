790d75f244b0ba06a2883c0e37c204c1fcdac48b
dardouk.amjad <fprintfamjad@gmail.com>
2017-11-08 13:24:47 +0200

    first commit

---
+++ src/project/Project.java
@@ -0,0 +1,25 @@
+
+package project;
+
+import java.awt.FlowLayout;
+import javax.swing.*;
+
+/**
+ *
+ * @author amjad
+ */
+public class Project {
+public static MainFrame f;
+
+    public static void main(String[] args) {
+        f=new MainFrame();
+      
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.validate();
+       
+        
+      
+    }
+    
+}
